458bbd400547591380df4e1c0baee6b488c2eee9
sandbhorsarjerao@gmail.com <sandbhorsarjerao@gmail.com>
2020-12-13 14:18:58 +0530

    Added Test4 into feature1 Branch

---
+++ src/test/java/com/qa/tests/LoginTest4.java
@@ -0,0 +1,14 @@
+package com.qa.tests;
+
+// Test4 file added from Eclipse
+
+import org.testng.annotations.Test;
+
+public class LoginTest4 
+{	
+	@Test
+	public void test1()
+	{
+		System.out.println("test1");
+	}
+}
